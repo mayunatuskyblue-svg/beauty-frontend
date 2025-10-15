32d59e0925e59ac6a275cdd3d3ce793c01e0cdc4
mayunatuskyblue-svg <mayunatu.skyblue@gmail.com>
2025-10-15 18:21:20 +0900

    Update _middleware.js

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,12 +1,22 @@
 // functions/_middleware.js
 export const onRequest = async ({ request, next }) => {
-  const response = await next();
+  // まず通常通り配信
+  const res = await next();
   const url = new URL(request.url);
 
-  // /assets/css/ 配下の .css ファイルだけ MIME 修正
+  // /assets/css/*.css だけ Content-Type を強制
   if (url.pathname.startsWith('/assets/css/') && url.pathname.endsWith('.css')) {
-    response.headers.set('Content-Type', 'text/css; charset=utf-8');
+    // 既存ヘッダをコピーして上書き
+    const headers = new Headers(res.headers);
+    headers.set('content-type', 'text/css; charset=utf-8');
+
+    // Response を作り直して返す（←ここがポイント）
+    return new Response(res.body, {
+      status: res.status,
+      statusText: res.statusText,
+      headers
+    });
   }
 
-  return response;
+  return res;
 };
